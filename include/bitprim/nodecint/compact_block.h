f614a7bcd2c462da9b488ec57f2c7e0d9aa31d9a
Dario Ramos <dario.fenix@gmail.com>
2017-06-29 18:43:00 -0300

    Source comments for compact_block.h

--- include/bitprim/nodecint/compact_block.h
+++ include/bitprim/nodecint/compact_block.h
@@ -15,6 +15,10 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @file compact_block.h
+ * @brief Functions for manipulating a compact block from the blockchain
+ *
  */
 
 #ifndef BITPRIM_NODE_CINT_COMPACT_BLOCK_H
@@ -32,27 +36,67 @@ extern "C" {
 
 //TODO Dario check how to map compact_block::short_ids()
 
+/**
+ * @brief Get compact block header
+ * @param block Handle to a compact block instance
+ * @return Handle to the compact block's header. Must be released by calling header_destruct()
+ */
 BITPRIM_EXPORT
 header_t compact_block_header(compact_block_t block);
 
+/**
+ * @brief Determine if a compact block is valid
+ * @param block Handle to a compact block instance
+ * @return True (non zero) iif the compact block is valid
+ */
 BITPRIM_EXPORT
 int /*bool*/ compact_block_is_valid(compact_block_t block);
 
+/**
+ * @brief Get a compact block's serialized size
+ * @param block Handle to a compact block instance
+ * @param version Protocol version
+ * @return The compact block's serialized size
+ */
 BITPRIM_EXPORT
 size_t compact_block_serialized_size(compact_block_t block, uint32_t version);
 
+/**
+ * @brief Get the compact block's transaction count
+ * @param block Handle to a compact block instance
+ * @return Amount of transactions that comprise the compact block
+ */
 BITPRIM_EXPORT
 size_t compact_block_transaction_count(compact_block_t block);
 
+/**
+ * @brief Get a compact block's n-th transaction
+ * @param block Handle to a compact block instance
+ * @param n Index to the transaction inside the block. Starts at 0 (zero) for the first one
+ * @return Handle to the n-th transaction. Must be released by calling transaction_destruct()
+ */
 BITPRIM_EXPORT
 transaction_t compact_block_transaction_nth(compact_block_t block, size_t n);
 
+/**
+ * @brief Get compact block's nonce
+ * @param block Handle to a compact block instance
+ * @return Compact block's nonce
+ */
 BITPRIM_EXPORT
 uint64_t compact_block_nonce(compact_block_t block);
 
+/**
+ * @brief Release memory held by compact block instance
+ * @param block Handle to a compact block instance
+ */
 BITPRIM_EXPORT
 void compact_block_destruct(compact_block_t block);
 
+/**
+ * @brief Reset compact block
+ * @param block Handle to a compact block instance
+ */
 BITPRIM_EXPORT
 void compact_block_reset(compact_block_t block);
 
