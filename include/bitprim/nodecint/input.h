d61ae45968f86243674b65dcd746de6ca9b8bb39
Dario Ramos <dario.fenix@gmail.com>
2017-06-29 19:16:46 -0300

    Source comments for input.h

--- include/bitprim/nodecint/input.h
+++ include/bitprim/nodecint/input.h
@@ -15,6 +15,10 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @file input.h
+ * @brief Functions for manipulating a transaction's input
+ *
  */
 
 #ifndef BITPRIM_NODE_CINT_INPUT_H_
@@ -30,27 +34,68 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Release the memory held by an input instance
+ * @param input Handle to an input instance
+ */
 BITPRIM_EXPORT
 void input_destruct(input_t input);
 
+/**
+ * @brief Determine if an input is valid
+ * @param input Handle to an input instance
+ * @return True (non zero) iif the input is valid
+ */
 BITPRIM_EXPORT
 int input_is_valid(input_t input);
 
+/**
+ * @brief Determine if an input is final
+ * @param input Handle to an input instance
+ * @return True (non zero) iif the input is final
+ */
 BITPRIM_EXPORT
 int /*bool*/ input_is_final(input_t input);
 
+/**
+ * @brief Get the input's serialized size
+ * @param input Handle to an input instance
+ * @param wire Deprecated; currently ignored
+ * @return Input's serialized size
+ */
 BITPRIM_EXPORT
 size_t input_serialized_size(input_t input, int wire /* = true*/);
 
+/**
+ * @brief Get the input's sequence number
+ * @param input Handle to an input instance
+ * @return The input's sequence number
+ */
 BITPRIM_EXPORT
 uint32_t input_sequence(input_t input);
 
+/**
+ * @brief Get the input's signature operations count
+ * @param input Handle to an input instance
+ * @param bip16_active If true (non zero), consider only active operations
+ * @return Signature operations count
+ */
 BITPRIM_EXPORT
 size_t input_signature_operations(input_t input, int /*bool*/ bip16_active);
 
+/**
+ * @brief Get input script
+ * @param input Handle to an input instance
+ * @return Handle to input script. Must be released by calling script_destruct()
+ */
 BITPRIM_EXPORT
 script_t input_script(input_t input);
 
+/**
+ * @brief Get the input's previous output
+ * @param input Handle to an input instance
+ * @return Handle to input's previous output. Must be released by calling output_point_destruct
+ */
 BITPRIM_EXPORT
 output_point_t input_previous_output(input_t input);
 
